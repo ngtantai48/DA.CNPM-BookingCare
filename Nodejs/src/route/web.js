c451ae26dc3e004d16da6ada8eaaa0b4f93b3988
Nguyễn Tấn Tài <ngtantai48@gmail.com>
2024-03-29 00:10:55 +0700

    Apply the MVC model to the project

--- Nodejs/src/route/web.js
+++ Nodejs/src/route/web.js
@@ -1,11 +1,14 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello world with CogangTeam");
-    });
+    router.get("/", homeController.getHomePage);
+    router.get("/about", homeController.getAboutPage);
+
+    //rest api
+    
     return app.use("/", router);
 }
 
