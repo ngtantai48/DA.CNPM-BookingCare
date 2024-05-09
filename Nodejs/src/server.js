7ee5a944388e38de4ce7eb43f7ca2f5b5713ae86
Thanhtrung271101 <npttrung2711@gmail.com>
2024-05-09 18:25:09 +0700

    + complete login ReactJs and NodeJs

--- Nodejs/src/server.js
+++ Nodejs/src/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import cors from 'cors';
+
 require('dotenv').config();
 
 let app = express();
+app.use(cors({ credentials: true, origin: true }));
 
 //config app
 
@@ -13,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
 
 let port = process.env.PORT || 6969;
 //PORT === undefined => port = 6969
