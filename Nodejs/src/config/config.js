77330203270d82b1821ffa14e29128a6d88d1cb9
Nguyen Tan Tai <ngtantai48@gmail.com>
2024-05-13 00:23:32 +0700

    + fix env

---
+++ Nodejs/src/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST,
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_PRODUCTION,
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  }
+};
