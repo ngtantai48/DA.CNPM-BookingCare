0bab92c58fa1251c6bef79e4c198fbdd96da89c6
XTrung1 <lexuantrung674219@gmail.com>
2024-05-14 21:45:13 +0700

    test commit video 52

--- Nodejs/src/route/web.js
+++ Nodejs/src/route/web.js
@@ -1,6 +1,6 @@
 import express from "express";
 import homeController from "../controllers/homeController";
-import userController from "../controllers/userController"
+import userController from "../controllers/userController";
 
 let router = express.Router();
 
@@ -15,12 +15,16 @@ let initWebRoutes = (app) => {
   router.get("/delete-crud", homeController.deleteCRUD);
   //rest api
 
-  router.post('/api/login', userController.handleLogin);
-  router.get('/api/get-all-users', userController.handleGetAllUsers);
-  router.post('/api/create-new-user', userController.handleCreateNewUser);
-  router.put('/api/edit-user', userController.handleEditUser)
-  router.delete('/api/delete-user', userController.handleDeleteUser)
-  
+  router.post("/api/login", userController.handleLogin);
+  router.get("/api/get-all-users", userController.handleGetAllUsers);
+  router.post("/api/create-new-user", userController.handleCreateNewUser);
+  router.put("/api/edit-user", userController.handleEditUser);
+  router.delete("/api/delete-user", userController.handleDeleteUser);
+
+  router.post("/api/login", userController.handleLogin);
+
+  router.get("/allcode", userController.getAllCode);
+
   return app.use("/", router);
 };
 
